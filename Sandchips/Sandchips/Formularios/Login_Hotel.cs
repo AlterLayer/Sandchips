1f6c3fe7c1692c4e8891544ddf8bdd8ebb9c84a2
Diego Ramírez Martínez <dramirez@cgclatam.com>
2018-07-27 00:01:39 -0600

    Revert "//se elimino un comentario"
    This reverts commit 2e1191fda30dcb1aa17750d38983c5a1031aeda8.

--- Sandchips/Sandchips/Formularios/Login_Hotel.cs
+++ Sandchips/Sandchips/Formularios/Login_Hotel.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 using Sandchips;
 using Sandchips.DAL;
 using Sandchips.Formularios;
@@ -19,6 +20,9 @@ namespace Sandchips.Formularios
     {
         public Login_Hotel()
         {
+            //
+            // Required for Windows Form Designer support
+            //
             InitializeComponent();
         }
 
